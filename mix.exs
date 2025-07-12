7be15938392c3357830b63837dd3566a45e55bab
lATAl <hi@anhtuan.me>
2025-07-12 10:22:21 +0700

    feat: update to latest rustler and tiktoken-rs

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule Tiktoken.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
-      {:rustler, "~> 0.30.0"},
+      {:rustler, "~> 0.36.0"},
       {:rustler_precompiled, "~> 0.8"}
     ]
   end
